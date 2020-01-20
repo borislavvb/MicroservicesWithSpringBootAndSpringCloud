e021f0e5681c77a779cfdf80fea7ab6872327a75
borislavvb <borislav.borisov32@gmail.com>
2020-01-20 14:36:07 +0200

    Created User package that contains User, UserDaoService and UserController classes and has the functionality to create a new user, to retrieve all users and retrieve a user by id

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/user/UserController.java
@@ -0,0 +1,44 @@
+package com.in28minutes.rest.webservices.restfulwebservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserDaoService userDaoService;
+
+    @Autowired
+    public UserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        User user = userDaoService.findOne(id);
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser = userDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
