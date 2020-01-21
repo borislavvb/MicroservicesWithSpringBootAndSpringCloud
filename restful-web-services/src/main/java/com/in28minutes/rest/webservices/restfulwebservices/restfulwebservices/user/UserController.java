d6b6d93641cb6c2f8d88b908b4d4a58e38fc41ea
borislavvb <borislav.borisov32@gmail.com>
2020-01-21 14:48:57 +0200

    Created deleteUserById endpoint in UserController and deleteById method in UserDaoService

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/user/UserController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/user/UserController.java
@@ -5,10 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.lang.reflect.Array;
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Stream;
 
 @RestController
 public class UserController {
@@ -21,28 +19,30 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return userDaoService.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public User retrieveUser(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
-        if(user==null){
-            throw new UserNotFoundException(String.format("User with id %d does not exist!",id));
+        if (user == null) {
+            throw new UserNotFoundException(String.format("User with id %d does not exist!", id));
         }
 
         return user;
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user){
-        userDaoService.findAll().stream()
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        userDaoService.findAll()
                 .forEach(x -> {
-                    if(x.getName().equals(user.getName())){
-                       throw new UserAlreadyExist(String.format("User with name %s already exist!",user.getName()));
-                    }});
+                    if (x.getName().equals(user.getName())) {
+                        throw new UserAlreadyExistException(String.format("User with name %s already exist!", user.getName()));
+                    }
+                });
+
         User savedUser = userDaoService.save(user);
 
         URI location = ServletUriComponentsBuilder
@@ -51,5 +51,14 @@ public class UserController {
                 .buildAndExpand(savedUser.getId()).toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = userDaoService.deleteById(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("User with id %d does not exist!", id));
+        }
     }
 }
