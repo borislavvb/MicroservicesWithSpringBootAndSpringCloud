7007c3ce9a0847d6b616b22a6bd00dcbbc271be8
borislavvb <borislav.borisov32@gmail.com>
2020-01-22 15:22:40 +0200

    Implementing support for xml formats in hello-world controller

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/configuration/LocationLanguageConfig.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/configuration/LocationLanguageConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -13,15 +14,8 @@ public class LocationLanguageConfig {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        AcceptHeaderLocaleResolver sessionLocaleResolver = new AcceptHeaderLocaleResolver();
         sessionLocaleResolver.setDefaultLocale(Locale.US);
         return sessionLocaleResolver;
-    }
-
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        return messageSource;
     }
 }
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.restfulwebservices.h
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class HelloWorldController {
     }
 
     @GetMapping("/hello-world/internationalization")
-    public String helloWorldInternationalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalization() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
