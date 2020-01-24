832c9ac27937f2ff360320c5a1fab32974d28d1d
borislavvb <borislav.borisov32@gmail.com>
2020-01-24 18:28:05 +0200

    added static filtering

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/restfulwebservices/user/User.java
@@ -1,17 +1,23 @@
 package com.in28minutes.rest.webservices.restfulwebservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user!")
 public class User{
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters!")
+    @ApiModelProperty(notes = "The name should be at least 2 characters!")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "The data should be in the past!")
     private Date birthDate;
 
     public User(){
